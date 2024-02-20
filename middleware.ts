52bda37c376d1d1d9f4f0dc77ce43f1b6911b58e
pane2004 <alexlu2000clash@gmail.com>
2024-02-19 21:00:39 -0500

    middleware logic upgrade

--- middleware.ts
+++ middleware.ts
@@ -4,14 +4,25 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   const currentToken = request.cookies.get("access_token")?.value;
 
-  // user is attempting to access non-public route without token, redirect
-  if (!currentToken) {
-    const response = NextResponse.redirect(new URL("/", request.url));
+  // redirect user to public if trying to access anything in private
+  if (request.nextUrl.pathname.startsWith("/events-private") && !currentToken) {
+    const response = NextResponse.redirect(new URL("/events-public", request.url));
+    return response;
+  }
+
+  // request all events-public requests to private if user is logged in
+  if (request.nextUrl.pathname.startsWith("/events-public") && currentToken) {
+    const { searchParams } = request.nextUrl;
+    const url = new URL("/events-private", request.url);
+    searchParams.forEach((value, key) => {
+      url.searchParams.set(key, value);
+    });
+    const response =  NextResponse.redirect(url);
     return response;
   }
 }
 
 // run middleware on the private routes
 export const config = {
-  matcher: ["/events-private/:path*"],
+  matcher: ["/events-private/:path*", "/events-public/:path*"],
 };
